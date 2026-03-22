de9dcbe9fb19110d227fd94e8d256192620689ac
potomnazovy <skeleton39876@mail.ru>
2026-03-22 18:13:38 +0300

    [KV-DAS] Define all structures for FSM

--- main.cpp
+++ main.cpp
@@ -1,4 +1,36 @@
 #include <iostream>
+#include <stdexcept>
+#include <cstring>
+#include <cstddef>
+
+struct Transition
+{
+  char symbol;
+  const char* toState;
+};
+
+struct State
+{
+  const char* name;
+  const Transition* transitions;
+  int transitionsCount;
+};
+
+class FSM
+{
+private:
+  const char* alphabet;
+  const State* states;
+  int statesCount;
+  const char* startState;
+  const char** finiteStatesNames;
+  int finiteStatesCount;
+  const char* currentState;
+public:
+  FSM(const char* alphabet, const State* states, int statesCount,
+    const char* startState, const char** finiteStates, int finiteStatesCount);
+  bool test(const char* value);
+};
 
 int main()
 {
