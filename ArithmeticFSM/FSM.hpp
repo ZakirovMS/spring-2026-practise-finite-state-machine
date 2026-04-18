2e9ea19b83ab86146a6d308c0bc8e07342c0bf14
potomnazovy <skeleton39876@mail.ru>
2026-04-18 18:10:38 +0300

    [KV-AFC] add new file with fsm and classes for fsm State, InputType and Action

---
+++ ArithmeticFSM/FSM.hpp
@@ -0,0 +1,42 @@
+#ifndef FSM_H
+#define FSM_H
+
+#include <string>
+#include <utility>
+#include <vector>
+
+enum class State
+{
+  Start,
+  Number,
+  Operator,
+  Open_bracket,
+  Close_bracket,
+  Unary_minus,
+  Error
+};
+
+enum class InputType
+{
+  Digit,
+  Dot,
+  Bin_op,
+  Un_op,
+  Lbracket,
+  Rbracket,
+  Lnprefix,
+  Ignored,
+  Unknown,
+  End
+};
+
+enum class Action
+{
+  Push,
+  Push_marker,
+  Skip,
+  Set_error,
+  Continue
+};
+
+#endif
