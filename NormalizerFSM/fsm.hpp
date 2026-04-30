89b51907e3ffd4a2155895196aadab7c010de79e
potomnazovy <skeleton39876@mail.ru>
2026-04-30 20:32:02 +0300

    [KV-AES] add enum and struct for fsm actionб inputtype and state and transition

---
+++ NormalizerFSM/fsm.hpp
@@ -0,0 +1,46 @@
+#ifndef FSM_HPP
+#define FSM_HPP
+#include <string>
+
+enum class State
+{
+  Start,
+  Number,
+  Operator,
+  Open_bracket,
+  Close_bracket,
+  Unary_minus,
+  Error
+};
+
+enum class InputType
+{
+  Digit,
+  Dot,
+  Bin_op,
+  Un_op,
+  Lbracket,
+  Rbracket,
+  Lnprefix,
+  Ignored,
+  Unknown,
+  End
+};
+
+enum class Action
+{
+  Push,
+  Push_marker,
+  Skip,
+  Set_error,
+  Continue
+};
+
+struct Transition
+{
+  State nextstate;
+  Action action;
+  std::string token;
+};
+
+#endif
